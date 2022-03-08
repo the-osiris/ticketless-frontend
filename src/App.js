fa87ba305fe7e5a31f3ff63b72e74e133e68d76e
vamshi5421 <75656830+vamshi5421@users.noreply.github.com>
2022-03-08 17:28:04 +0530

    sdjnf

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Navbar } from './components/Navbar/Navbar';
-import HomeScreen from './screens/HomeScreen/HomeScreen'
-
+// import LoginForm from './components/LoginForm/LoginForm';
+import HomeScreen from './screens/HomeScreen/HomeScreen';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Navbar />
+
       <HomeScreen />
     </>
   );
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,7 +4,7 @@ import classes from './Button.module.css'
 const Button = () => {
   return (
     <>
-        <div className={classes.button}>Button</div>
+        <div className={classes.button}>Login</div>
     </>
   )
 }
